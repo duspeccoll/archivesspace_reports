a2576901aa01ff694f9fb47bebc9f345c208ba2c
Kevin Clair <kevin@jackflaps.net>
2018-06-13 11:29:49 -0600

    add column for representative status

--- archival_objects/archival_object_digitization_report/archival_object_digitization_report.rb
+++ archival_objects/archival_object_digitization_report/archival_object_digitization_report.rb
@@ -3,7 +3,13 @@ class ArchivalObjectDigitizationReport < AbstractReport
   register_report
 
   def headers
-    ['uri', 'title', 'component_id', 'location', 'url']
+    ['uri', 'title', 'component_id', 'location', 'url', 'is_representative']
+  end
+
+  def processor
+    {
+      'is_representative' => proc {|record| record[:representative].nil? ? false : true}
+    }
   end
 
   def query
@@ -18,7 +24,8 @@ class ArchivalObjectDigitizationReport < AbstractReport
       .left_outer_join(:instance_do_link_rlshp, :instance_id => :instance__id)
       .left_outer_join(:digital_object, :id => :instance_do_link_rlshp__digital_object_id)
       .select(Sequel.as(:instance__archival_object_id, :id),
-              Sequel.as(:digital_object__digital_object_id, :url))
+              Sequel.as(:digital_object__digital_object_id, :url),
+              Sequel.as(:instance__is_representative, :is_representative))
 
     db[:archival_object]
       .left_outer_join(locations, {:id => :archival_object__id}, :table_alias => :location)
@@ -27,7 +34,8 @@ class ArchivalObjectDigitizationReport < AbstractReport
               Sequel.as(:archival_object__title, :title),
               Sequel.as(:archival_object__component_id, :component_id),
               Sequel.as(:location__location, :location),
-              Sequel.as(:digital_object__url, :url))
+              Sequel.as(:digital_object__url, :url),
+              Sequel.as(:digital_object__is_representative, :representative))
       .filter(:archival_object__repo_id => repo_id)
       .exclude(:location__location => nil)
   end
